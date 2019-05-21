60c8702d8ad9d881ff1349dc9b9d2e998aaeca0e
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-05-21 23:04:55 +0530

    github repo url added

--- constants/api.js
+++ constants/api.js
@@ -1,6 +1,7 @@
 const api = {
   'REPO_DETAILS': 'https://api.github.com/repos/{username}/{repostiory}',
-  'GITHUB_CONTRIBUTORS': 'https://api.github.com/repos/{username}/{repository}/contributors'
+  'GITHUB_CONTRIBUTORS': 'https://api.github.com/repos/{username}/{repository}/contributors',
+  'GITHUB_REPO': 'https://github.com/{username}/{repository}'
 };
 
 
